41ca968fdf6eb26b8ca268e8dcd7522c1aef6940
chapssrijan <srijan.chapagain@cou.fi>
2015-07-01 14:32:14 +0300

    xzcasdfc

---
+++ Assets/Kamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kamera : MonoBehaviour {
+	GameObject target;
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag ("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 follow = target.transform.position;
+		follow.z = transform.position.z;
+		transform.position = follow;
+	
+	}
+}
